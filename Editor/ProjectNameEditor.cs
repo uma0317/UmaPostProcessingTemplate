bc35db85d158b400eb160ade045d944706ccde34
uma <blog3170@gmail.com>
2019-11-25 17:11:13 +0900

    init

---
+++ Editor/ProjectNameEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+
+namespace Uma
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(ProjectName))]
+    public class ProjectNameEditor : Editor
+    {
+        SerializedProperty _mode;
+
+        void OnEnable()
+        {
+            _mode = serializedObject.FindProperty("_mode");
+
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(_mode);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
+
---
+++ Runtime/ProjectName.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Uma
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+    [AddComponentMenu("Uma Image Effects/Project Name")]
+    public class ProjectName : MonoBehaviour
+    {
+        #region Public Properties
+        [SerializeField, Range(0, 3)]
+        int _mode = 0;
+
+        public int mode
+        {
+            get { return _mode; }
+            set { _mode = value; }
+        }
+        #endregion
+
+        #region Private Properties
+        [SerializeField] Shader _shader;
+        Material _material;
+        #endregion
+
+        #region MonoBehaviour Functions
+        private void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            if (_material == null)
+            {
+                _material = new Material(_shader);
+                _material.hideFlags = HideFlags.DontSave;
+            }
+            Graphics.Blit(source, destination, _material);
+        }
+        #endregion
+    }
+}
